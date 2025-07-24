d5a3ae4c4f3bb98a95f6f5d77bafa3d5d25be18e
pratik <pratikchavan11296@gmail.com>
2025-07-24 11:59:22 +0530

    product page and navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navigation from './customer/components/Navigation/Navigation'
+// import Navigation from './customer/components/Navigation/Navigation'
+//import { Navigation } from './customer/components/Navigation/navigation.js';
+import Navigation from './customer/components/Navigation/Navigation';
+import Product from './customer/components/Product/Product';
+
+
 import HomePage from './customer/pages/HomePage/HomePage'
 import Footer from './customer/components/Footer/Footer'
 
@@ -14,7 +19,8 @@ function App() {
       <div>
         <Navigation/>
         <div>
-          <HomePage/>
+          {/* <HomePage/> */}
+          <Product/>
         </div>
         <Footer/>
       </div>
