5d3c42e3d0960f67c7f90fca6281f77cc57ac5e4
Falguni Sahare <falgunisahare01.com>
2025-08-01 14:49:24 +0530

    Frontend of Cart and CartItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,15 @@ import './App.css'
 //import { Navigation } from './customer/components/Navigation/navigation.js';
 import Navigation from './customer/components/Navigation/Navigation';
 import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
+import Cart from './customer/components/Cart/Cart';
+import Checkout from './customer/components/Checkout/Checkout';
 
 
 import HomePage from './customer/pages/HomePage/HomePage'
 import Footer from './customer/components/Footer/Footer'
+import AddressCard from './customer/components/AddressCard/AddressCard';
+import OrderSummary from './customer/components/Checkout/OrderSummary';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,8 +24,15 @@ function App() {
       <div>
         <Navigation/>
         <div>
-          {/* <HomePage/> */}
-          <Product/>
+         {/* <HomePage/> */} 
+          {/* <Product/>  */}
+           {/* <ProductDetails/>  */}
+          {/* <Cart/> 
+          <Checkout/> */}
+          {/* <AddressCard/> */}
+          <OrderSummary/>
+
+          
         </div>
         <Footer/>
       </div>
